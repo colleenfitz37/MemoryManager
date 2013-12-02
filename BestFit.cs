a8576188b8c575bf20392d8592c943e0d1721779
colleenfitz37 <colleenfitz37@yahoo.com>
2013-12-02 18:53:49 -0500

    Update BestFit.cs

--- BestFit.cs
+++ BestFit.cs
@@ -15,7 +15,7 @@ namespace MemoryManager
         public BestFit(int totalMemory, int OSMemory)
             : base(totalMemory, OSMemory)
         {
-
+        
         }
 
         /// <summary>
@@ -25,6 +25,40 @@ namespace MemoryManager
         /// <returns>RETURNS A LIST REPRESENTING MEMORY</returns>
         public override List<string> FitProcess(Process processToAdd) 
         {
+            
+            int gapSize = 0;
+            int gapStart = 0;
+            int smallestGap = 0;
+            string[] temp = memorySpots.ToArray<string>();
+            foreach (KeyValuePair<int, Process> p in addedProcesses)
+            {
+                for (int i = 0; i < p.Value.size; i++)
+                {
+                    temp[p.Key + i] = "1 " + p.Value.PID;
+                }
+            }
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i].Substring(0, 1).Equals("0"))
+                {
+                    smallestGap = gapSize;
+                }
+                
+                // I'm not sure what youre analyzing with this part... I am trying to mod you worst fit program
+                
+                if ((temp[i].Substring(0, 1).Equals("1")) || (i >= temp.Length - 1))
+                {
+                    if (gapSize > smallestGap)
+                    {
+                        smallestGap = gapSize;
+                        gapStart = i - gapSize;
+                    }
+                    else
+                    {
+                        gapSize = 0;
+                    }
+                }
             return base.FitProcess(processToAdd);
         }
 
